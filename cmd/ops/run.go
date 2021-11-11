269fede62b9e8c8d7a55d91031016d265cadb557
jettjia <jettjia@qq.com>
2021-11-11 14:13:35 +0800

    fate install

--- cmd/ops/run.go
+++ cmd/ops/run.go
@@ -1,9 +1,11 @@
 package ops
 
+import "github.com/jettjia/go-micro-frame-cli/cmd/ops/service/mysql"
+
 func RunOps(args []string) {
 	serviceName := args[0]
 
 	if serviceName == "mysql" {
-
+		mysql.RunMysql()
 	}
 }
